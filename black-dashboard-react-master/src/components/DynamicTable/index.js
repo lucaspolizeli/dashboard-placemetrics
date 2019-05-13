a1ddfc479c04aa47cacabd7400ecec413d2f779f
Lucas Polizeli <lumpolizeli@gmail.com>
2019-05-13 01:53:21 -0300

    Finish all site

--- black-dashboard-react-master/src/components/DynamicTable/index.js
+++ black-dashboard-react-master/src/components/DynamicTable/index.js
@@ -5,21 +5,26 @@ import PropTypes from "prop-types";
 function DynamicTable({ dataToTable, size, title, columnHeader }) {
   //FIXME: Refactor to be full dynamic
   function renderRows() {
-    if (!dataToTable || !dataToTable.lenght) return [];
+    let dataToTableInHTML = [];
 
-    return dataToTable.map(currentData => (
-      <tr>
-        <td>{currentData.temperature}</td>
-        <td>{currentData.humidity}</td>
-        <td>{currentData.createdAt.split("T")[0]}</td>
-        <td>{currentData.createdAt.split(".")[0]}</td>
-      </tr>
-    ));
+    for (let i = 0; i < dataToTable.length; i++) {
+      const currentData = dataToTable[i];
+      if (i === 15) break;
+
+      dataToTableInHTML.push(
+        <tr key={i}>
+          <td>{currentData.temperature}</td>
+          <td>{currentData.humidity}</td>
+          <td>{currentData.createdAt.split("T")[0]}</td>
+          <td>{currentData.createdAt.split(".")[0].split("T")[1]}</td>
+        </tr>
+      );
+    }
+
+    return dataToTableInHTML.reverse();
   }
 
   function renderColumnHeader() {
-    if (!columnHeader || !columnHeader.lenght) return [];
-
     return columnHeader.map(currentHeader => <th>{currentHeader}</th>);
   }
 
--- black-dashboard-react-master/src/views/DataHistory.jsx
+++ black-dashboard-react-master/src/views/DataHistory.jsx
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Row } from "reactstrap";
+import DynamicTable from "../components/DynamicTable";
+import api from "./../services/api";
+import CardToViewValue from "./../components/CardToViewValue";
 
-class Tables extends React.Component {
-  render() {
-    return (
-      <>
-        <div className="content">
-          <Row />
-        </div>
-      </>
-    );
+function DataHistory() {
+  const [dataFromService, setDataFromService] = useState([]);
+
+  useEffect(() => {
+    async function onLoad() {
+      const responseFromAPI = await getDataFromService();
+      setDataFromService(responseFromAPI);
+    }
+
+    onLoad();
+
+    setInterval(() => {
+      onLoad();
+    }, 4000);
+  }, []);
+
+  async function getDataFromService() {
+    const responseFromAPI = await api.get("/");
+    if (responseFromAPI.status === 200) {
+      return responseFromAPI.data.userData;
+    }
+
+    return [];
   }
+
+  return (
+    <>
+      <div className="content">
+        <Row>
+          <CardToViewValue
+            size={4}
+            title="Quantidade de Envios"
+            icon="tim-icons icon-cloud-download-93"
+            value={dataFromService.length}
+          />
+          <DynamicTable
+            size={12}
+            title="Histórico de Envios"
+            columnHeader={["Temperatura", "Umidade", "Data", "Hora"]}
+            dataToTable={dataFromService}
+          />
+        </Row>
+      </div>
+    </>
+  );
 }
 
-export default Tables;
+export default DataHistory;
